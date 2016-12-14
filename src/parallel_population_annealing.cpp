ebc5f4ea1d906b49048bfa4ff8bea74eaa8b5508
Chris Pattison <chpattison@gmail.com>
2016-12-14 06:36:00 -0600

    Hack implementation of time varying population.

--- src/parallel_population_annealing.cpp
+++ src/parallel_population_annealing.cpp
@@ -25,6 +25,7 @@ ParallelPopulationAnnealing::ParallelPopulationAnnealing(Graph& structure, std::
 PopulationAnnealing(structure, schedule, 0, seed) {
     
     average_node_population_ = average_population / parallel_.size();
+    init_average_node_population_ = average_node_population_;
     average_population_ = average_node_population_ * parallel_.size();
     
     if(seed == 0) {
@@ -200,6 +201,23 @@ std::vector<ParallelPopulationAnnealing::Result> ParallelPopulationAnnealing::Ru
 }
 
 double ParallelPopulationAnnealing::Resample(double new_beta) {
+    double new_population = 
+    4.92531500e-04    * std::pow(new_beta, 10)
+    + -1.33979484e-02 * std::pow(new_beta, 9)
+    + 1.56855509e-01  * std::pow(new_beta, 8)
+    + -1.03197763e+00 * std::pow(new_beta, 7)
+    + 4.17595505e+00  * std::pow(new_beta, 6)
+    + -1.06759251e+01 * std::pow(new_beta, 5)
+    + 1.70092672e+01  * std::pow(new_beta, 4)
+    + -1.59838117e+01 * std::pow(new_beta, 3)
+    + 7.96914062e+00  * std::pow(new_beta, 2)
+    + -2.01506291e+00 * new_beta
+    + 9.93379099e-01;
+
+    average_population_ *= new_population;
+    average_node_population_ = average_population_ / parallel_.size();
+    average_population_ = average_node_population_ * parallel_.size();
+
     std::vector<StateVector> resampled_replicas;
     std::vector<int> resampled_families;
     resampled_replicas.reserve(replicas_.size());
@@ -365,7 +383,7 @@ std::vector<double> ParallelPopulationAnnealing::FamilyCount() {
     std::vector<parallel::Packet<Family>> packets;
     
     for(auto& f : families) {
-        int source_rank = f.tag / average_node_population_;
+        int source_rank = f.tag / init_average_node_population_;
         if(source_rank != parallel_.rank()) {
             auto it = std::find_if(packets.begin(), packets.end(), [&](const parallel::Packet<Family> p) { return p.rank == source_rank; });
             if(it != packets.end()) {
